074b205221ee15a610b1f0f046d392a5486c21ef
Joskemu <vadhelkemu098@gmail.com>
2025-02-16 10:36:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+it('types and checks the input value in uppercase', () => {
+  cy.visit(baseUrl); // Visit your URL where this HTML is hosted
+  cy.get('#fname').type('jOhN dOe');
+  cy.get('body').click(); // Blur the input field
+  cy.get('#fname').should('have.value', 'JOHN DOE'); // Ensure the value is uppercase
+});
